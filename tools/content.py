836e9c825ab7d3211605d78fc9bd3db883c7afd2
Your Name <you@example.com>
2022-11-05 15:43:41 +0300

    0.1.4

--- tools/content.py
+++ tools/content.py
@@ -1,4 +1,4 @@
 EnvVarsNotFound = "env vars {} is None"
 ErrorIsExists = "Login: {} already exists"
-ErrorBodyJSON = "json must contain: login, password, role, data"
+ErrorBodyJSON = "json must contain: fname, lname, login, password, role, data"
 ErrorData = "JSON is Null"
